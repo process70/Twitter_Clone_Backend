05cea15deb79ab6644ff3e06fe877e2a0adbf583
process70 <163150253+process70@users.noreply.github.com>
2024-11-11 19:33:59 +0000

    Update index.js

--- index.js
+++ index.js
@@ -23,7 +23,8 @@ app.use(cors({
     "http://localhost:5173",
     "https://twitter-clone-frontend-phi.vercel.app",
     "https://twitter-clone-frontend-6nts.onrender.com"
-  ]
+  ],
+  credentials: true,
 }));
 
 cloudinary.config({
